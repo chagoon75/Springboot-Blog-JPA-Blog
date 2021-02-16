2cc93e90be243348c818e6d402a8787f7bed4f36
chagoon75 <chagoon75@gmail.com>
2021-02-17 01:48:38 +0900

    Git Test

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-
+	//깃테스트
 	@GetMapping({"","/"})
 	public String index( ) {
 		// /WEB-INF/views/index.jsp
