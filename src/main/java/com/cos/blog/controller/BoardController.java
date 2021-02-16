2f9f4d5d69d1619f8c6534a2d79d3212518cae95
chagoon75 <chagoon75@gmail.com>
2021-02-17 02:26:57 +0900

    local 5th Modify

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,12 +1,10 @@
 package com.cos.blog.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-	// Git Local Modify 3
-	@GetMapping({"","/"})
+	// Git Local Modify 35	@GetMapping({"","/"})
 	public String index( ) {
 		// /WEB-INF/views/index.jsp
 		return "index";
