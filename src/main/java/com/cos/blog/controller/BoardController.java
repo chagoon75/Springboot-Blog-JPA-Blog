336e56d7ae01fa8232940c4efe94a87ad4eee4a2
chagoon75 <77916653+chagoon75@users.noreply.github.com>
2021-02-17 02:18:36 +0900

    Update BoardController.java

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
-	// Git Local Modify2
+	// Git Local Modify 3
 	@GetMapping({"","/"})
 	public String index( ) {
 		// /WEB-INF/views/index.jsp
