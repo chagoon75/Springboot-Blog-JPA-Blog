e719fbe965a7f22c9c3fe4576dc49a085693ca79
chagoon75 <77916653+chagoon75@users.noreply.github.com>
2021-03-08 10:46:22 +0900

    Update BlogControllerTest.java

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -3,6 +3,7 @@ package com.cos.blog.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// GitHub(master) 에서 수정 - 2021.03.08 1);45
 // GitHub && 로컬 Merge 완료. - 2021.03.08 10:33
 // 스프링이 com.cos.blog패키지 이하를 스캔해서 모든파일을 메노리에 new하는것은 아니구요.
 // 특정 어노테이션이 붙어있는 클래스 파일들이 new헤서 (IoC) 스프링 컨테이너에 관리해줍니다.  
