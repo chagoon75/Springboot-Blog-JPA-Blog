f8a4ab872f4eb4be6bf30fbf5c0d33d8e694a021
chagoon75 <chagoon75@gmail.com>
2021-03-08 10:19:25 +0900

    로컬commit - 2021.03.08 10:19

--- src/main/java/com/cos/blog/test/BlogControllerTest.java
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-// 로컬에서 수정한 내용입니다.
+// 로컬에서 수정한 내용입니다. - 2021.03.08 10:02
 //스프링이 com.cos.blog패키지 이하를 스캔해서 모든파일을 메노리에 new하는것은 아니구요.
 //특정 어노테이션이 붙어있는 클래스 파일들이 new헤서 (IoC) 스프링 컨테이너에 관리해줍니다.  
 @RestController                     
