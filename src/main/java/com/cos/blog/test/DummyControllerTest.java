3d2e84f8e1aed79b2c3976ed4b5975422b18be0f
chagoon75 <chagoon75@gmail.com>
2021-03-08 10:39:30 +0900

    develop brench 수정 commit - 2021.03.08 10:39

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -21,6 +21,7 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+// develop brech 로컬수정 - 2021.03.08 10:38
 // GitHub && 로컬 Merge 완료. - 2021.03.08 10:33
 // html파일이 아니라 date를 리터해주는 controller = RestController
 @RestController
